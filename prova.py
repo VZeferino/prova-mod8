c6dfb24de90059ad820f08465bd34fb06e601da3
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2023-11-17 14:47:54 -0300

    feat: prova

---
+++ prova.py
@@ -0,0 +1,57 @@
+#! /bin/env python3
+
+import re
+
+# Dicionário de intenções
+intent_patterns = {
+    'option1': re.compile(r'Quero consultar minha (.*)'), #testado com 'Quero consultar minha atualização de pagamento'
+    'option2': re.compile(r'Quero consultar o (.*)'), #testado com 'Quero consultar o status do pedido'
+}
+
+# Dicionário de ações
+def responseUpdate(prompt):
+    print(f"Entendido, consultarei a sua {prompt}")
+
+def responseFollow(prompt):
+    print(f"Entendido, consultarei o {prompt}")
+
+action_mapping = {
+    'option1': responseUpdate,
+    'option2': responseFollow,
+}
+
+def process_intent(user_input):
+    for intent, pattern in intent_patterns.items():
+        match = pattern.match(user_input)
+        if match:
+            return intent, match.group(1)
+    return None, None
+
+def execute_action(intent, prompt):
+    if intent in action_mapping:
+        action = action_mapping[intent]
+        action(prompt)
+        return True
+    return False
+
+def main():
+    loop = True
+
+    while loop == True:
+
+        user_input = input(">> ")
+        if user_input == "sair":
+            loop = False
+        else:
+            intent, prompt = process_intent(user_input)
+            if intent:
+                success = execute_action(intent, prompt)
+                if success:
+                    print('Ligue para a central e será informado, seu problema é grave!')
+                else:
+                    print('Ação não reconhecida.')
+            else:
+                print('Comando não compreendido.')
+
+if __name__ == '__main__':
+    main()
